088c86d9c038fe18ac0812d82ff653876d2ea519
Timpan42 <77719322+Timpan42@users.noreply.github.com>
2022-10-21 13:38:06 +0200

    Modelen funkar, har ingen view eller controller som funkar.

--- src/controller.java
+++ src/controller.java
@@ -1,2 +1,14 @@
-public class controller {
+import javax.swing.*;
+
+public class controller extends JFrame {
+    model model;
+    view view;
+
+    public controller(model m, view v){
+        this.model = m;
+        this.view = v;
+
+
+    }
+
 }
